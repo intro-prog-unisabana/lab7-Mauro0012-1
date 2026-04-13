dc6b3f5bc821905f5e7ccdbe5ff0eedcf94746ae
Mauro <andresmoor@unisaba.edu.co>
2026-04-12 23:15:17 -0500

    1

--- password_manager.py
+++ password_manager.py
@@ -4,20 +4,46 @@ from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
-    """TODO: Parte 1."""
-    pass
+    with open(filename, "r") as f:
+        password = f.read().strip()
+    encrypted_password=caesar_encrypt(password)
+    with open(filename, "w") as f:
+        f.write(encrypted_password)
+        
+    
 
 
 def encrypt_passwords_in_file(filename: str) -> None:
-    """TODO: Parte 2."""
-    pass
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader if row]
+    for index, row in enumerate(rows):
+        if index!= 0:
+            row[2] = caesar_encrypt(row[2])
+
+    with open(filename, "w", newline="") as f:
+         writer = csv.writer(f)
+         writer.writerows(rows)
+
 
 
 def change_password(filename: str, website: str, password: str) -> bool:
-    """TODO: Parte 3."""
-    pass
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader if row]
+    for index, row in enumerate(rows):
+        if row[0] == website:
+            rows[index][2] = caesar_encrypt(password)
+            with open(filename, "w", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerows(rows)
+            return True   
+    return False
+
 
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
-    """TODO: Parte 4."""
-    pass
+    encrypted_password = caesar_encrypt(password)
+    with open(filename, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([website_name, username, encrypted_password])
